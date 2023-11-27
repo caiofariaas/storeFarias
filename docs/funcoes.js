c3730a4fd13664f9ce9454df1afa3eb7337cc739
caiofariaas <caiofarias077@gmail.com>
2023-11-27 14:24:05 -0300

    newJS

--- docs/funcoes.js
+++ docs/funcoes.js
@@ -213,33 +213,72 @@ export function totalCart(carrinho_compras) {
     right_bar.innerHTML += cart_total;
 }
 
+
+
+// Mostrar produto filtrado
+
+// export function mostrarProdFIltrados(produtosFiltrados) {
+
+//     produtosFiltrados.forEach(prod => {
+//         const products_grid = document.getElementById("products_grid");
+
+//         let cardProd = `
+//             <div class="row" id="${prod.id}">
+//                 <a href="produtoUnic.html"><img src="${prod.img}" id="${prod.id}"></a>
+//                 <div class="product-text">
+//                     <h5>New</h5>
+//                 </div>
+//                 <div class="preco">
+//                     <h4>${prod.nomeProd}</h4>
+//                     <p>R$${prod.precoProd.toFixed(2).replace('.', ',')}</p>
+//                 </div>
+//             </div>`;
+
+//         products_grid.innerHTML += cardProd;
+//     });
+
+// };
+
+
 // Barra de Busca
 
-const searchIcon = document.querySelector('.searchIcon')
 
-searchIcon.addEventListener('click', (e) =>{
+export function searchBar(){
+
+    const searchIcon = document.querySelector('.searchIcon')
+
+    searchIcon.addEventListener('click', (e) =>{
 
     e.preventDefault();
 
     let searchTerm = document.querySelector('#search-box2').value;
     console.log(searchTerm);
 
-    let produtosFiltrados = catalogo.filter(produto => produto.nomeProd.toLowerCase().includes(searchTerm.toLowerCase()));
+    let produtosFiltrados = catalogo.filter((produto) => {
+        const produtoNomeLowerCase = produto.nomeProd.toLowerCase();
+        const searchTermLowerCase = searchTerm.toLowerCase();
+
+        return produtoNomeLowerCase.includes(searchTermLowerCase);
+    });
+
 
     document.querySelector('#search-box2').value = '';
+
     console.log(produtosFiltrados)
 
- if (!isCurrentPage('produtos.html')) {
+    if (!isCurrentPage('produtos.html')) {
 
-// Transforma os produtos filtrados em uma string JSON para ser passada como parâmetro na URL
+        // Transforma os produtos filtrados em uma string JSON para ser passada como parâmetro na URL
 
         let produtosFiltradosString = encodeURIComponent(JSON.stringify(produtosFiltrados));
 
-// Redireciona para a nova página com a barra de consulta contendo os produtos filtrados
+        // Chama a função para mostrar os produtos filtrados
+        // Redireciona para a nova página com a barra de consulta contendo os produtos filtrados
 
         window.location.href = 'produtos.html?produtos=' + produtosFiltradosString;
     }
 });
+}
 
 // Função para verificar se já está na página de destino
 
